7eeab5f9e8ed09d235455e626f67f28494611416
Sangyong Sim <sangyong-sim@cookpad.com>
2018-09-04 21:52:27 +0900

    MySQL cookbook

---
+++ cookbooks/mysql/default.rb
@@ -0,0 +1,12 @@
+service 'mysql'
+
+%w(
+  /etc/mysql/my.cnf
+).each do |file|
+  remote_file file do
+    owner 'root'
+    group 'root'
+    mode '644'
+    notifies :restart, 'service[mysql]'
+  end
+end
