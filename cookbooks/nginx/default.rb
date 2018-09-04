c7d715352aecf52b28e3f1dd71e5a8efc7c2dd1a
Sangyong Sim <sangyong-sim@cookpad.com>
2018-09-04 21:55:09 +0900

    Add nginx cookbook

---
+++ cookbooks/nginx/default.rb
@@ -0,0 +1,12 @@
+package 'nginx'
+
+service 'nginx' do
+  action :enable
+end
+
+remote_file '/etc/nginx/nginx.conf' do
+  owner 'root'
+  group 'root'
+  mode '644'
+  notifies :reload, 'service[nginx]'
+end
