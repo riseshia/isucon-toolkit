0965d5ff69987741d3b1da75cf678df06d81495c
Sangyong Sim <sangyong-sim@cookpad.com>
2018-09-04 21:46:15 +0900

    Add deploy script

---
+++ README.md
@@ -0,0 +1,36 @@
+## Bootstrap
+
+`cookbook/deploy-script/default.rb` に `<repository_url>`, `<deploy_to>` 設定
+
+## Itamae
+
+```bash
+bin/itamae <aliased server> <cookbook name>
+```
+
+## Initialize log
+
+```bash
+bin/init_logs
+```
+
+## Analyze
+
+```bash
+bin/access_log # nginx log
+bin/slow_log   # mysql slow log
+```
+
+## Deploy
+
+### deploy-script cookbook
+
+```bash
+bin/itamae <aliased server> deploy-script
+```
+
+### Go deploy
+
+```bash
+bin/deploy <aliased server>
+```
---
+++ cookbooks/deploy-script/default.rb
@@ -0,0 +1,35 @@
+deploy_to = ""
+repository_url = ""
+ruby_bin_path = "/home/isucon/local/ruby/bin"
+restart_command = "systemctl restart isubata.ruby.service"
+
+file "/home/isucon/deploy_remote" do
+  mode "755"
+  owner "root"
+  group "root"
+
+  content <<~EOS
+  #!/bin/bash
+
+  set -ex
+
+  BRANCH=${1:-master}
+
+  PATH=/home/isucon/local/ruby/bin:$PATH
+
+  cd /home/isucon
+  if [ ! -d "/home/isucon/src" ]; then
+    git clone #{repository_url} src
+  fi
+
+  cd /home/isucon/src
+  git fetch origin
+  git reset --hard origin/$BRANCH
+  ln -sf /home/isucon/src/ruby #{deploy_to}
+
+  cd #{deploy_to}
+  #{ruby_bin_path}/bundle check || #{ruby_bin_path}/bundle install
+
+  sudo #{restart_command}
+  EOS
+end
